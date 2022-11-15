cf4e21677e0e6a08454b2aaf8bc391ed80883dbe
mnemoli <mnemoli@users.noreply.github.com>
2022-11-15 12:24:42 +0000

    Use enums

--- DLLTest/Sprite_Brus_D2.cpp
+++ DLLTest/Sprite_Brus_D2.cpp
@@ -2,6 +2,8 @@
 #include "Sprite_Brus_D2.h"
 #include <random>
 
+using namespace Petz;
+
 Sprite_Brus_D2::~Sprite_Brus_D2()
 {
 }
@@ -12,18 +14,18 @@ int randrange(int min, int max) {
 
 void Sprite_Brus_D2::InitToyExtraCode(bool param_1, Host* host)
 {
-	SetAdjValue((EAdj)2, 6);
-	SetAdjValue((EAdj)6, 5);
-	SetAdjValue((EAdj)7, 12);
-	SetAdjValue((EAdj)8, randrange(40, 50));
-	SetAdjValue((EAdj)9, randrange(55, 65));
-	SetAdjValue((EAdj)10, randrange(29, 33));
-	SetAdjValue((EAdj)19, randrange(15, 25));
-	SetAdjValue((EAdj)23, randrange(15, 25));
-	SetAdjValue((EAdj)24, randrange(15, 25));
-	SetAdjValue((EAdj)26, randrange(15, 25));
-	SetAdjValue((EAdj)29, randrange(90, 100));
-	SetAdjValue((EAdj)26, randrange(90, 100));
+	SetAdjValue(Toyz, 6);
+	SetAdjValue(Color, Blue);
+	SetAdjValue(Flavor, Wood);
+	SetAdjValue(Size, randrange(40, 50));
+	SetAdjValue(Mass, randrange(55, 65));
+	SetAdjValue(Friction, randrange(29, 33));
+	SetAdjValue(Chew, randrange(15, 25));
+	SetAdjValue(EAdj::Soft, randrange(15, 25));
+	SetAdjValue(Fuzzy, randrange(15, 25));
+	SetAdjValue(Bounce, randrange(15, 25));
+	SetAdjValue(Vain, randrange(90, 100));
+	SetAdjValue(Groom, randrange(90, 100));
 }
 
 void Sprite_Brus_D2::PlayFoleySound(EFoleySound sound, EFlavor flavor)
